82ca08d20b7378bdab5262193d703b6f5c139008
CSELonewolf <devkantgorai1@gmail.com>
2021-12-15 02:53:54 +0530

    configure the views to dynamically fetch the product in the home.html page

--- greatcart/greatcart/urls.py
+++ greatcart/greatcart/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 from django.conf.urls.static import static
@@ -8,5 +8,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home,name='home'),
+    path('store/', include('store.urls')),
 ]+ static(settings.MEDIA_URL ,document_root=settings.MEDIA_ROOT)
--- greatcart/greatcart/views.py
+++ greatcart/greatcart/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
-	return render(request,'home.html')
+	products = Product.objects.all().filter(is_available = True)
+	context = {
+		'products': products
+	}
+	return render(request,'home.html',context)
